b770a709b1034500ea1950fd34fc8303c4862b15
humstarman <lueyoung7@gmail.com>
2018-05-30 15:24:37 +0800

    init

---
+++ check-ansible.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [ ! -x "command -v ansible" ]; then
+  echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [WARN] - no ansible found, start installing."
+  if [ -x "$(command -v apt-get)" ]; then
+    apt-get update
+    apt-get install -y software-properties-common
+    apt-add-repository -y ppa:ansible/ansible
+    apt-get update
+    apt-get install -y ansible
+  fi
+  if [ -x "$(command -v yum)" ]; then
+    yum makecache
+    yum install -y ansible
+  fi
+  echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [INFO] - ansible installed."
+else
+  echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [INFO] - ansible already existed."
+fi 
