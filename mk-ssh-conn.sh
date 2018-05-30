50213f66ff94343811b85758991efd865c11d480
humstarman <lueyoung7@gmail.com>
2018-05-30 16:23:31 +0800

    update: rm wrong id

--- mk-ssh-conn.sh
+++ mk-ssh-conn.sh
@@ -4,11 +4,11 @@ if [ -z "$PASSWD" ]; then
   echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [ERROR] - need the password." 
 fi
 if [ ! -x "$(command -v expect)" ]; then
-  if [ ! -x "$(command -v apt-get)" ]; then
+  if [ -x "$(command -v apt-get)" ]; then
     apt-get update
     apt-get install -y tcl tk expect
   fi
-  if [ ! -x "$(command -v yum)" ]; then
+  if [ -x "$(command -v yum)" ]; then
     yum makecache
     yum install -y tcl tk expect
   fi
