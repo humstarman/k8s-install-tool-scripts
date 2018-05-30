e74604cfe4b145f52c810e24239c497c4a3d375f
humstarman <lueyoung7@gmail.com>
2018-05-30 16:57:33 +0800

    init

--- check-master-node.sh
+++ check-master-node.sh
@@ -7,10 +7,8 @@ if [ ! -f ./master.csv ]; then
 fi
 echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [INFO] - master configured."
 if [ ! -f ./node.csv ]; then
-  echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [ERROR] - no node config file -- node.csv found!"
-  echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [ERROR] - put node info into ./node.csv in term of CSV."
-  sleep 3
-  exit 1
+  echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [WARN] - no node config file -- node.csv found!"
+else
+  echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [INFO] - node configured."
 fi
-echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [INFO] - node configured."
 exit 0
